ec51191b0227dae7d2ae47649a5ebb24ed7f7147
Ritisha12 <68296139+Ritisha12@users.noreply.github.com>
2020-08-19 14:29:59 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C27-project-trial-2
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,59 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint=Matter.Constraint
+var bob1,bob2,bob3,bob4,bob5
+var roof
+var bobDiameter
+var rope1,rope2,rope3,rope4,rope5
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+   bob1=new Bob(290,450,50)
+   bob2=new Bob(340,450,50)
+   bob3=new Bob(390,450,50)
+   bob4=new Bob(440,450,50)
+   bob5=new Bob(490,450,50)
+   roof=new Roof(400,200,400,25)
+   rope1-new Rope(bob1.body,roof.body,bobDiameter*2,0)
+   rope2-new Rope(bob2.body,roof.body,bobDiameter*2,0)
+   rope3-new Rope(bob3.body,roof.body,bobDiameter*2,0)
+   rope4-new Rope(bob4.body,roof.body,bobDiameter*2,0)
+   rope5-new Rope(bob5.body,roof.body,bobDiameter*2,0)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  bob1.display()
+  bob2.display()
+  bob3.display()
+  bob4.display()
+  bob5.display()
+  roof.display()
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()
+  drawSprites();
+ 
+}
+
+
+
